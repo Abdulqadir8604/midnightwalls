234e167c81134aab26181f2a12b6020069139076
Abdulqadir <abdulqadir.b8604@gmail.com>
2022-09-04 14:41:01 +0530

    update

--- app/src/main/java/com/lamaq/midnightwalls/Adapter.kt
+++ app/src/main/java/com/lamaq/midnightwalls/Adapter.kt
@@ -1,21 +1,31 @@
 package com.lamaq.midnightwalls
 
+import android.app.WallpaperManager
+import android.content.ContentProvider
+import android.content.ContentUris
 import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.net.Uri.parse
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.res.FontResourcesParserCompat.parse
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.LazyHeaders
 import com.google.firebase.firestore.Query
+import com.squareup.okhttp.HttpUrl.parse
+import java.net.HttpCookie.parse
+import java.net.URI
+import java.util.logging.Level.parse
 
 
 // Extends the Adapter class to RecyclerView.Adapter
@@ -37,10 +47,10 @@ class Adapter     // Constructor for initialization
     // Binding data to the into specified position
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // TypeCast Object to int type
-        val glideUrl = GlideUrl(image[position] as String, LazyHeaders.Builder()
+        val imageUrl = GlideUrl(image[position] as String, LazyHeaders.Builder()
             .addHeader("User-Agent", "your-user-agent")
             .build())
-        Glide.with(context).load(glideUrl).placeholder(R.drawable.image_placeholder).into(holder.images)
+        Glide.with(context).load(imageUrl).placeholder(R.drawable.image_placeholder).into(holder.images)
 
         holder.images.setOnClickListener {
             val intent = Intent(context, WallpaperActivity::class.java)
--- app/src/main/java/com/lamaq/midnightwalls/WallpaperActivity.kt
+++ app/src/main/java/com/lamaq/midnightwalls/WallpaperActivity.kt
@@ -1,12 +1,15 @@
 package com.lamaq.midnightwalls
 
 import android.app.WallpaperManager
+import android.content.ContentUris
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.provider.MediaStore
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
@@ -14,17 +17,18 @@ import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import java.io.IOException
 import java.io.InputStream
 import java.net.HttpURLConnection
+import java.net.URI
 import java.net.URL
 
 
 class WallpaperActivity : AppCompatActivity(){
     private var wallpaperUrl = ""
-    private val progressBar: ProgressBar by lazy { findViewById(R.id.progressBar) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -41,7 +45,7 @@ class WallpaperActivity : AppCompatActivity(){
 
         Glide.with(this)
             .load(wallpaperUrl)
-            .override(SIZE_ORIGINAL, SIZE_ORIGINAL)
+            .fitCenter()
             .into(imageView)
 
 
@@ -51,11 +55,11 @@ class WallpaperActivity : AppCompatActivity(){
         }
     }
 
-    private fun setWallpaper(url: String) {
+    private fun setWallpaper(uri: String) {
         val policy = ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
         val wallpaperManager = WallpaperManager.getInstance(this)
-        val bitmap:Bitmap? = getBitmapFromURL(url)
+        val bitmap:Bitmap? = getBitmapFromURL(uri)
         try {
             wallpaperManager.setBitmap(bitmap)
             Toast.makeText(this, "Wallpaper set!", Toast.LENGTH_SHORT).show()
